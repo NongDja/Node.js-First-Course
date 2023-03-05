ce18127b98ee649feb5721a6c41b52dd89c8efb1
nongdforwork <113155849+NongDja@users.noreply.github.com>
2023-03-05 16:17:35 +0700

    all

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const debug = require('debug')('app');
-const morgan = require('morgan');
+const express = require("express");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
+const genericRouter = express.Router();
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get('/', (req, res) => {
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-    res.send('Hello IamDew Kub!');
+genericRouter.route("/").get((req, res) => {
+  res.render("generics");
+});
 
-})
+genericRouter.route("/1").get((req, res) => {
+  res.send("Hellow world!! 11");
+});
 
-app.listen(port, () => {
-    debug(`Example app listening at http://localhost:${port}`);
-})
+app.use("/generics", genericRouter);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: "NongDew",
+    customers: ["Jedsadaporn", "John", "Jane"],
+  });
+});
+
+app.listen(PORT, () => {
+  debug(`Example app listening at http://localhost:${PORT}`);
+});
